ffeef875be43ebb29c7ebadbda3ef322cef6c9b4
rado <radoslav192@gmail.com>
2018-05-28 14:52:10 +0300

    Made it faster.

--- data_structures/clever_adding_intervals_set.cpp
+++ data_structures/clever_adding_intervals_set.cpp
@@ -9,59 +9,45 @@ template<class T, class T2> inline int chkmax(T &x, const T2 &y) { return x < y 
 template<class T, class T2> inline int chkmin(T &x, const T2 &y) { return x > y ? x = y, 1 : 0; }
 const int MAXN = (1 << 20);
 
+template<class T>
 struct clever_set
 {
-	set<pair<int, int> > st;
-	map<pair<int, int>, int> value;
+	map<pair<int, int>, T> value;
 
-	void init(int n) { st.insert({n, 1}); value[{n, 1}] = -1; }
+	void init(int n) { value[{n, 1}] = (T)0; }
 
-	vector<pair<pair<int, int>, int> > add(int l, int r, int val)
+	vector<pair<pair<int, int>, T> > add(int l, int r, T val)
 	{
-		auto bg = *st.lower_bound({l, 0});
+		auto bg = value.lower_bound({l, 0})->first;
 		if(bg.second != l)
 		{
-			int val = value[bg];
-			st.erase(bg);
+			T val = value[bg];
 			value.erase(bg);
-
 			value[{l - 1, bg.second}] = val;
-			st.insert({l - 1, bg.second});
-
 			value[{bg.first, l}] = val;
-			st.insert({bg.first, l});
 		}
 
-		auto en = *st.lower_bound({r, 0});
+		auto en = value.lower_bound({r, 0})->first;
 		if(en.first != r)
 		{
-			int val = value[en];
-			st.erase(en);
+			T val = value[en];
 			value.erase(en);
-
 			value[{en.first, r + 1}] = val;
-			st.insert({en.first, r + 1});
-
 			value[{r, en.second}] = val;
-			st.insert({r, en.second});
 		}
 		
-		vector<pair<pair<int, int>, int> > ret;
-		auto it = st.lower_bound({l, 0});
+		vector<pair<pair<int, int>, T> > ret;
+		auto itt = value.lower_bound({l, 0});
 		while(true)
 		{
-			if(it == st.end() || it->first > r) break;
-			ret.push_back({{it->second, it->first}, value[*it]});
-			++it;
+			if(itt == value.end() || itt->first.first > r) break;
+			ret.push_back({{itt->first.second, itt->first.first}, itt->second});
+			++itt;
 		}
 
 		for(auto it: ret) 
-		{
-			st.erase({it.first.second, it.first.first});
 			value.erase({it.first.second, it.first.first});
-		}
 
-		st.insert({r, l});
 		value[{r, l}] = val;
 		return ret;
 	}

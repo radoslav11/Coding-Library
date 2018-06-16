3f5a591335743c7dc5b114311d87c2e0fc61db3a
rado <radoslav192@gmail.com>
2018-06-16 10:21:10 +0300

    BCC

--- graph/bcc.cpp
+++ graph/bcc.cpp
@@ -2,86 +2,87 @@
 #define endl '\n'
 
 using namespace std;
+template<class T, class T2> inline int chkmax(T &x, const T2 &y) { return x < y ? x = y, 1 : 0; }
+template<class T, class T2> inline int chkmin(T &x, const T2 &y) { return x > y ? x = y, 1 : 0; }
 const int MAXN = (1 << 20);
 
 int n, m;
-vector<int> G[MAXN];
+vector<int> adj[MAXN];
 
 void read()
 {
 	cin >> n >> m;
-
 	for(int i = 0; i < m; i++)
 	{
 		int u, v;
 		cin >> u >> v;
-
-		G[u].push_back(v);
-		G[v].push_back(u);
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
 }
 
-stack<int> st;
-int low[MAXN], disc[MAXN];
-int dfs_time, bcc[MAXN], comps = 0;
+int low[MAXN], disc[MAXN], dfs_time;
+vector<vector<pair<int, int> > > bcc;
+vector<pair<int, int> > st, bridges;
 
-void dfs(int u, int par)
+void tarjan(int u, int pr = -1)
 {
-	low[u] = dfs_time;
-	disc[u] = dfs_time;
-	dfs_time++;
-
-	st.push(u);
-
-	int sz = G[u].size(), v;
-	for(int i = 0; i < sz; i++)
-	{
-		v = G[u][i];
-		if(disc[v] == -1)
+	low[u] = disc[u] = ++dfs_time;
+	
+	int child_cnt = 0;
+	for(int v: adj[u])
+		if(v != pr)
 		{
-			dfs(v, u);
-			low[u] = min(low[u], low[v]);
-		}
-		else if(v != par)
-			low[u] = min(low[u], disc[v]);
-	}
+			if(disc[v] == -1)
+			{
+				st.push_back({u, v});
+				tarjan(v, u);
+				child_cnt++;
+				chkmin(low[u], low[v]);
+			
+				if((pr == -1 && child_cnt > 1) || (pr != -1 && disc[u] <= low[v]))
+				{
+					vector<pair<int, int> > curr = {make_pair(u, v)};
+					while(st.back() != make_pair(u, v))
+					{
+						curr.push_back(st.back());
+						st.pop_back();
+					}	
 
-	if(low[u] == disc[u])
-	{
-		comps++;
-		while(st.top() != u)
-		{
-			bcc[st.top()] = comps;
-			st.pop();
+					st.pop_back();
+					if(curr.size() == 1) bridges.push_back(curr.back());
+					else bcc.push_back(curr);
+				}
+			}
+			else if(disc[v] < disc[u])
+			{
+				chkmin(low[u], disc[v]);
+				st.push_back({u, v});
+			}
 		}
-
-		bcc[u] = comps;
-		st.pop();
-	}
 }
 
-void tarjan()
+void compute_bcc()
 {
-	memset(disc, -1, sizeof(disc));
-	memset(low, -1, sizeof(low));
-	memset(bcc, -1, sizeof(low));
 	dfs_time = 0;
-	comps = 0;
-
-	while(!st.empty())
-		st.pop();
-
+	for(int i = 1; i <= n; i++) low[i] = disc[i] = -1;
 	for(int i = 1; i <= n; i++)
 		if(disc[i] == -1)
-			dfs(i, -1);
+		{
+			st.clear();
+			tarjan(i);
+			if(!st.empty())
+			{
+				if(st.size() == 1) bridges.push_back(st.back());
+				else bcc.push_back(st);
+				st.clear();
+			}
+		}	
 }
 
 void solve()
 {
-	tarjan();
 
-    for(int i = 1; i <= n; i++)
-        cout << i << " " << bcc[i] << endl;
 }
 
 int main()

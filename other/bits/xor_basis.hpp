c9daa1616423df2993774b9a038a2eb858eaf47f
radoslav11 <radoslav.stoyanov.dimitrov@gmail.com>
2025-04-27 15:13:47 -0400

    Make xor_basis compressed

--- other/bits/xor_basis.hpp
+++ other/bits/xor_basis.hpp
@@ -5,23 +5,23 @@ template<class T, int num_bits>
 class xor_basis {
   public:
     int sz;
-    vector<T> base;
+    vector<T*> base;
 
     void clear() {
         sz = 0;
-        base.assign(num_bits, T());
+        base.assign(num_bits, nullptr);
     }
     xor_basis() { clear(); }
 
     void add(T val) {
         for(int i = num_bits - 1; i >= 0; i--) {
             if(get_bit(val, i)) {
-                if(is_zero(base[i])) {
-                    base[i] = val;
+                if(!base[i]) {
+                    base[i] = new T(val);
                     sz++;
                     return;
                 } else {
-                    val = xor_op(val, base[i]);
+                    val = xor_op(val, *base[i]);
                 }
             }
         }
@@ -32,8 +32,8 @@ class xor_basis {
     T max_xor() {
         T res = T();
         for(int i = num_bits - 1; i >= 0; i--) {
-            if(!get_bit(res, i) && !is_zero(base[i])) {
-                res = xor_op(res, base[i]);
+            if(!get_bit(res, i) && base[i]) {
+                res = xor_op(res, *base[i]);
             }
         }
 
@@ -42,8 +42,8 @@ class xor_basis {
 
     bool can_create(T val) {
         for(int i = num_bits - 1; i >= 0; i--) {
-            if(get_bit(val, i) && !is_zero(base[i])) {
-                val = xor_op(val, base[i]);
+            if(get_bit(val, i) && base[i]) {
+                val = xor_op(val, *base[i]);
             }
         }
         return is_zero(val);
@@ -52,8 +52,8 @@ class xor_basis {
     vector<T> get_basis() {
         vector<T> res;
         for(int i = 0; i < num_bits; i++) {
-            if(!is_zero(base[i])) {
-                res.push_back(base[i]);
+            if(!base[i]) {
+                res.push_back(*base[i]);
             }
         }
         return res;
@@ -66,8 +66,8 @@ class xor_basis {
 
         xor_basis<T, num_bits> res = *this;
         for(auto x: other.base) {
-            if(!is_zero(x)) {
-                res.add(x);
+            if(x) {
+                res.add(*x);
             }
         }
         return res;
